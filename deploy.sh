91da2e724ca54be6f6650bd84bb88d2e41ef287c
nssy <624029344@qq.com>
2021-06-08 15:52:02 +0800

    add choice master or slave to build

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,34 @@ echo "/*************************************************************************
 currentPath=$(cd `dirname $0`; pwd)
 echo "[INFO] current work path : $currentPath"
 
+mkdir $currentPath/service/data
+mkdir $currentPath/service/data/binlog
+
+echo "please input server id of mysql container, 1 means is the master"
+read mysqlServerId
+echo "mysqlServerId＝$mysqlServerId"
+if [ $mysqlServerId -eq 1 ];then 
+    cp $currentPath/service/config/docker-master.cnf $currentPath/service/config/docker.cnf
+    cp $currentPath/service/once_bin/master-init.pre $currentPath/service/once_bin/init.sql
+else 
+    cp $currentPath/service/config/docker-slave.cnf $currentPath/service/config/docker.cnf
+    cp $currentPath/service/once_bin/slave-init.pre $currentPath/service/once_bin/init.sql
+    echo "please input master sql address"
+    read masterAddress
+    echo "please input master sql port"
+    read masterPort
+    echo "please input sync account"
+    read syncAccount
+    echo "please input sync password"
+    read syncPassword
+    sed -i "s/masterAddress/$masterAddress/g" $currentPath/service/once_bin/init.sql
+    sed -i "s/masterPort/$masterPort/g" $currentPath/service/once_bin/init.sql
+    sed -i "s/syncAccount/$syncAccount/g" $currentPath/service/once_bin/init.sql
+    sed -i "s/syncPassword/$syncPassword/g" $currentPath/service/once_bin/init.sql
+fi
+
+sed -i "s/server-id=1/server-id=$mysqlServerId/g" $currentPath/service/config/docker.cnf
+
 # **************************** 容器配置 start  (根据实际情况修改) ********************
 # NOTE: Dockerfile生成的镜像的名称
 imageName="mariadb"
@@ -18,7 +46,7 @@ imageName="mariadb"
 imageVersion="latest"
 
 # NOTE: 启动容器的名称
-containerName="sql"
+containerName="sql_$mysqlServerId"
 # *************************************** end ***********************************
 
 # ******************************* 不需要修改的配置 start ***************************
@@ -60,7 +88,13 @@ checkDockerContainer() {
     else
         echo "[INFO] ----begin run $containerName container."
         
-        sudo docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v $hostVolumeConfigPath:$configPath -v $hostVolumeDataPath:$dataPath -v $hostVolumeAwaysBinPath:$awaysBinPath -v $hostVolumeOnceBinPath:$onceBinPath --restart always --name $containerName $imageNameVersion --init-file=$awaysBinPath/every_start_run.sql
+        if [ $mysqlServerId -eq 1 ];then 
+            echo 'begin running master'
+            sudo docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v $hostVolumeConfigPath:$configPath -v $hostVolumeDataPath:$dataPath -v $hostVolumeAwaysBinPath:$awaysBinPath -v $hostVolumeOnceBinPath:$onceBinPath --restart always --name $containerName $imageNameVersion --init-file=$awaysBinPath/every_start_run.sql
+        else 
+            echo 'begin running slave'
+            sudo docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v $hostVolumeConfigPath:$configPath -v $hostVolumeDataPath:$dataPath -v $hostVolumeAwaysBinPath:$awaysBinPath -v $hostVolumeOnceBinPath:$onceBinPath --restart always --name $containerName $imageNameVersion --init-file=$awaysBinPath/every_start_run.sql
+        fi
     fi
 }
 checkDockerContainer
